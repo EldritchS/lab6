ab4f9ffa35d7691357c1fab88663c5ed17150628
xovers-ufl <xovers@ufl.edu>
2024-10-22 19:39:35 -0400

    Update decode.py
    Change logic of the code to match what the assignment expects

--- decode.py
+++ decode.py
@@ -4,10 +4,26 @@ def decode(string):
 
     decoded_string = '' # declare global string variable
 
-    for char in string: # iterate through the characters of the string
-        char_num_val = ord(char) # convert character to it's number ascii number value
-        decoded_char_num_val = char_num_val + 3 # increase character's ascii number value by 3
-        new_char = chr(decoded_char_num_val) # convert back to a string character
-        decoded_string += new_char # add to new decoded string
+    for i in range(len(string)):
+        if string[i] == '0':
+            decoded_string += '7'
+        if string[i] == '1':
+            decoded_string += '8'
+        if string[i] == '2':
+            decoded_string += '9'
+        if string[i] == '3':
+            decoded_string += '0'
+        if string[i] == '4':
+            decoded_string += '1'
+        if string[i] == '5':
+            decoded_string += '2'
+        if string[i] == '6':
+            decoded_string += '3'
+        if string[i] == '7':
+            decoded_string += '4'
+        if string[i] == '8':
+            decoded_string += '5'
+        if string[i] == '9':
+            decoded_string += '6'
 
     return decoded_string
