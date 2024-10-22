c40b9d121ee55718c3cc9779eec9b4e673886a3a
xovers-ufl <xovers@ufl.edu>
2024-10-22 19:26:39 -0400

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,15 @@
+def decode(string):
+    """takes in the encoded password and returns the original password.
+    Written by Xavier Overs"""
+
+    decoded_string = '' # declare global string variable
+
+    for char in string: # iterate through the characters of the string
+        char_num_val = ord(char) # convert character to it's number ascii number value
+        decoded_char_num_val = char_num_val + 3 # increase character's ascii number value by 3
+        new_char = chr(decoded_char_num_val) # convert back to a string character
+        decoded_string += new_char # add to new decoded string
+
+    return decoded_string
+
+print(decode(input("Enter a string: ")))
